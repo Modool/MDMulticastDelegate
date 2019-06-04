947778ab21ba88a611808d418ba347e638ddca42
Elf Sundae <elf.sundae@gmail.com>
2019-06-04 23:06:21 +0800

    Remove unused import <AppKit/AppKit.h>

--- MDMulticastDelegate.m
+++ MDMulticastDelegate.m
@@ -1,9 +1,5 @@
 #import "MDMulticastDelegate.h"
 
-#if __has_feature(objc_arc_weak) && !TARGET_OS_IPHONE
-#import <AppKit/AppKit.h>
-#endif
-
 #if ! __has_feature(objc_arc)
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
